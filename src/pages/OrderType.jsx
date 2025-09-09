9f1c62350eb90f23da94ea07c52cdf5b4960803d
mariamsameh376 <romasameh580@gmail.com>
2025-09-09 02:30:44 -0700

    update in language map and safe

--- src/pages/OrderType.jsx
+++ src/pages/OrderType.jsx
@@ -42,6 +42,7 @@ export default function OrderType() {
             width="100%"
             height="100%"
             style={{ border: 0 }}
+            referrerpolicy="no-referrer-when-downgrade"
             allowFullScreen=""
             loading="lazy"
           ></iframe>
