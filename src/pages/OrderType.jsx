70b664687f3499f762698dbd212efe29e500d7f2
mariamsameh376 <romasameh580@gmail.com>
2025-09-09 02:24:43 -0700

    update in language map

--- src/pages/OrderType.jsx
+++ src/pages/OrderType.jsx
@@ -38,7 +38,7 @@ export default function OrderType() {
         <div className="w-full md:w-1/2 h-80 md:h-auto">
           <iframe
             title="Restaurant Location"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3221.1938153400642!2d-86.802389!3d36.161838!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886467674f9e6a91%3A0x9ed36a27636760ad!2sMidtown%20Foods!5e0!3m2!1sar!2sus!4v1756986028582!5m2!1sar!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3221.1936261916294!2d-86.80496898898583!3d36.16184260330906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886467674f9e6a91%3A0x9ed36a27636760ad!2sMidtown%20Foods!5e0!3m2!1sen!2seg!4v1757409665805!5m2!1sen!2seg&language=en&region=US"
             width="100%"
             height="100%"
             style={{ border: 0 }}
